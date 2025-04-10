cc117f79af58094e798293d9422857a8bde9642f
Abolfazl Borhani <abolfazlborhani80@gmail.com>
2025-04-10 17:28:13 +0330

    Edit folder  whi3perlib to whi3per

--- whi3perlib/cmake_utility/organize_source_files.cmake
+++
@@ -1,7 +0,0 @@
-file(GLOB source_files src/*.cc)
-file(GLOB public_header_files include/public/whi3perlib/*.hh)
-file(GLOB private_header_files include/private/whi3perlib/*.hh)
-
-source_group("Source Files" FILES ${source_files})
-source_group("Public Headers" FILES ${public_headers_files})
-source_group("Private Headers" FILES ${private_headers_files}) 
