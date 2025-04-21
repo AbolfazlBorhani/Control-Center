a09651a3b409ced657283ba068985789aa1c5c5f
WHI3PER <abolfazlborhani80@gmail.com>
2025-04-21 16:11:25 +0330

    Added <windows.h>, GetStdHandle(STD_OUTPUT_HANDLE)

--- whi3per/include/public/whi3per/controlcenter.hh
+++ whi3per/include/public/whi3per/controlcenter.hh
@@ -1,8 +1,11 @@
 #pragma once
 
+#include <windows.h>
 #include <string>
 
 namespace ControlCenter {
 	void RunProgram(int, char**);
 	void Help();
+
+	inline HANDLE hConsole{ GetStdHandle(STD_OUTPUT_HANDLE) };
 }
